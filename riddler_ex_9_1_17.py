c4e9c1f3a4741b3345babea52ccebfebf918b8a0
Christopher Shartrand <cshartrand@gmail.com>
2017-09-04 15:00:06 -0400

    Riddler express from 9/1/17

---
+++ riddler_ex_9_1_17.py
@@ -0,0 +1,19 @@
+#Five Thirty Eight Riddler Express Solution
+#by Chris Shartrand
+import numpy as np
+trials = 100000
+count_list = np.zeros(trials)
+for i in range(0,trials):
+    free_throw_count = 0
+    streak = 0
+    p = 0.7
+    while streak < 17:
+        make = np.random.binomial(1,p)
+        free_throw_count += 1
+        if make == 1:
+            streak += 1
+        else:
+            streak = 0
+    count_list[i] = free_throw_count
+average = np.mean(count_list) #approximately
+print average
